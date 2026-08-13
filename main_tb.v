db76757eaa1e4e7d701e253ee688e8b2363621c9
LUC-I-FER <87123844+LUC-I-FER@users.noreply.github.com>
2026-08-13 15:01:36 +0530

    Add testbench for seq_detector module
    This testbench simulates the seq_detector module, providing clock, reset, and input signals while displaying the output.

---
+++ main_tb.v
@@ -0,0 +1,52 @@
+module seq_detector_tb;
+  
+  reg clk;
+  reg rst;
+  reg in;
+  
+  wire out;
+  
+  seq_detector dut (.clk(clk), .rst(rst), .in(in), .out(out));
+  
+  always #5 clk = ~clk;
+  
+  task send_bit(input bit bit_value);
+    begin
+      @(negedge clk);
+      in = bit_value;
+      
+      @(posedge clk);
+      #1;
+      $display("Time=%0t | in = %b | out = %b", $time, in, out);
+    end
+  endtask
+  
+  initial begin
+    
+    clk = 0;
+    rst = 1;
+    in  = 0;
+    
+    @(posedge clk);
+    @(posedge clk);
+    
+    rst = 0;
+    send_bit(1);
+    send_bit(0);
+    send_bit(1);
+    send_bit(1);
+    send_bit(0);
+    send_bit(1);
+    send_bit(1);
+    send_bit(0);
+    send_bit(0);
+    send_bit(1);
+    send_bit(0);
+    send_bit(1);
+    send_bit(1);
+    
+    #10
+    $finish;
+  end
+  
+endmodule
